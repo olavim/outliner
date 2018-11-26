31c527fc054f53f7dc241a1ab96d8291d68674d4
Olavi Mustanoja <tilastokeskus@gmail.com>
2018-11-26 22:21:30 +0200

    refactor: better pdf block splitting

--- vendor.d.ts
+++ vendor.d.ts
@@ -1,4 +1,5 @@
 declare module '*.png'
+declare module '*.ttf'
 declare module 'src/client/serviceWorker'
 declare module 'serviceworker-webpack-plugin/lib/runtime';
 declare module 'serviceworker-webpack-plugin/lib/browser/registerEvents';
